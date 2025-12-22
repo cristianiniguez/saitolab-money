250ebffc40b7b9bc4dcb7ed31719f13cfa28eeec
cristianiniguez <felizpc1@gmail.com>
2025-12-21 21:19:21 -0400

    Only showing google button for now

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -1,5 +1,27 @@
-import { LoginForm } from '@/components/forms/login-form'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { Field, FieldGroup } from '@/components/ui/field'
+import { GoogleButton } from '@/components/google-button'
 
 export default function LoginPage() {
-  return <LoginForm />
+  return (
+    <Card>
+      <CardHeader className='text-center'>
+        <CardTitle className='text-xl'>Welcome back</CardTitle>
+        <CardDescription>Login with your Google account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <FieldGroup>
+          <Field>
+            <GoogleButton>Login with Google</GoogleButton>
+          </Field>
+        </FieldGroup>
+      </CardContent>
+    </Card>
+  )
 }
